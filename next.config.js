d9671935f06a02e64f24b6b49e50df3467817e71
cernojit <cernojit@users.noreply.github.com>
2020-09-23 12:05:39 +0200

    typo and email connect

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+}
